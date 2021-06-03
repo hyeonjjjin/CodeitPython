c77beea471766d27fcf58b1d8702ef45134bfb77
hjjja <jahj30734@gmail.com>
2021-06-03 12:10:35 +0900

    5_선수과제_

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 def is_palindrome(word):
-    for i in range(word):
+    temp1 = word[0:(len(word) / 2)]
+    temp2 = word[len(word) / 2 + 1 + (len(word) % 2):]
+    print(temp1)
+    print(temp2)
+    for i in range(len(temp1)):
         print()
 
 
 print(is_palindrome("racecar"))
-print(int(36/3))
-
+print(int(36 / 3))
