904c38f94192fd673b6673dd26273112344a3b91
hjjja <jahj30734@gmail.com>
2021-06-03 23:44:30 +0900

    이진탐색구현_

--- hello.py
+++ hello.py
@@ -10,10 +10,20 @@ def linear_search(element, some_list):
     return None
     # 코드를 작성하세요.
 
-print(linear_search(2, [2, 3, 5, 7, 11]))
-print(linear_search(0, [2, 3, 5, 7, 11]))
-print(linear_search(5, [2, 3, 5, 7, 11]))
-print(linear_search(3, [2, 3, 5, 7, 11]))
+def binary_search(element, some_list):
+    mid=int(len(some_list)/2)+(len(some_list)%2)
+    if some_list[mid] ==element:
+        return mid
+    elif some_list[mid] >element:
+        return binary_search(element, some_list[0:mid])
+    else: return binary_search(element, some_list[mid+1:])
+
+print(binary_search(2, [2, 3, 5, 7, 11]))
+print(binary_search(0, [2, 3, 5, 7, 11]))
+print(binary_search(5, [2, 3, 5, 7, 11]))
+print(binary_search(3, [2, 3, 5, 7, 11]))
+print(binary_search(11, [2, 3, 5, 7, 11]))
+
 print(linear_search(11, [2, 3, 5, 7, 11]))
 
 print(is_palindrome("hello"))
