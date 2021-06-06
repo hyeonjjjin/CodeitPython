4d0f01c0a487f98f3ad5eedc72a28fcf69fc9739
hjjja <jahj30734@gmail.com>
2021-06-06 23:25:18 +0900

    _

--- hello.py
+++ hello.py
@@ -18,6 +18,8 @@ def binary_search(element, some_list):
         return binary_search(element, some_list[0:mid])
     else: return binary_search(element, some_list[mid+1:])
 
+
+
 print(binary_search(2, [2, 3, 5, 7, 11]))
 print(binary_search(0, [2, 3, 5, 7, 11]))
 print(binary_search(5, [2, 3, 5, 7, 11]))
