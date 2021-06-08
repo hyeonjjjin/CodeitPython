017c15c0c852120784eb5d5dcfb63c80054b71a5
hjjja <jahj30734@gmail.com>
2021-06-08 22:16:24 +0900

    selection sort_fin

--- hello.py
+++ hello.py
@@ -20,15 +20,29 @@ def binary_search(element, some_list):
         elif some_list[mid] > element:
             mid = int(mid / 2)
         else:
-            mid += int(mid/2) + 1 - mid % 2
+            mid += int(mid / 2) + 1 - mid % 2
         if mid == 0 and some_list[mid] != element:
             return None
 
 
-print(binary_search(2, [2, 3, 5, 7, 11]))
-print(binary_search(0, [2, 3, 5, 7, 11]))
-print(binary_search(5, [2, 3, 5, 7, 11]))
-print(binary_search(3, [2, 3, 5, 7, 11]))
+def selection_sort(some_list):
+    for i in range(len(some_list)):
+        min = some_list[i]
+        swap = i
+        for j in range(i+1, len(some_list)):
+            if some_list[j] < min:
+                min = some_list[j]
+                swap = j
+        if swap != i:
+            tmp = some_list[i]
+            some_list[i] = some_list[swap]
+            some_list[swap] = tmp
+
+    return some_list
+
+
+print(selection_sort([6, 2, 7, 3, 1, 2]))
+
 print(binary_search(11, [2, 3, 5, 7, 11]))
 
 print(linear_search(11, [2, 3, 5, 7, 11]))
