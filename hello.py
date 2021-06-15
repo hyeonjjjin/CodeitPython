51efd745ae8163ef883d49ec39c1dfd2ea057909
hjjja <jahj30734@gmail.com>
2021-06-15 23:51:28 +0900

    max_product_ feedback

--- hello.py
+++ hello.py
@@ -150,6 +150,14 @@ def max_product(left_cards, right_cards):
     return max(all_mul)
 
 
+def max_product_feedback(left_cards, right_cards):
+    product = left_cards[0] * right_cards[0]
+    for left in left_cards:
+        for right in right_cards:
+            product = max(max_product, left * right)
+    return product
+
+
 print(max_product([1, 6, 5], [4, 2, 3]))
 print(max_product([1, -9, 3, 4], [2, 8, 3, 1]))
 print(max_product([-1, -7, 3], [-4, 3, 6]))
