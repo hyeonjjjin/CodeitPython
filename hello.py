339d1c43376d08f47c59d666f295a191c4db5bba
hjjja <jahj30734@gmail.com>
2021-06-17 23:49:29 +0900

    _

--- hello.py
+++ hello.py
@@ -177,7 +177,6 @@ def max_product_feedback(left_cards, right_cards):
 def trapping_rain(buildings):
 
 
-
 print(trapping_rain([3, 0, 0, 2, 0, 4]))
 print(trapping_rain([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
 
