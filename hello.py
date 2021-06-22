61be3bc871e23b08df0dc4ad96f2a6413d47216f
hjjja <jahj30734@gmail.com>
2021-06-23 00:37:06 +0900

    merge_sort__

--- hello.py
+++ hello.py
@@ -249,13 +249,17 @@ def merge_feedback(list1, list2):
     return merged_list
 
 
-# 테스트
+def merge_sort(my_list):
+    return merge(sorted(my_list[:len(my_list)//2]), sorted(my_list[len(my_list)//2:]))
+
+
+print(merge_sort([1, 3, 5, 7, 9, 11, 13, 11]))
+print(merge_sort([28, 13, 9, 30, 1, 48, 5, 7, 15]))
+print(merge_sort([2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]))
+
 print(merge([1], []))
-print(merge([], [1]))
-print(merge([2], [1]))
 print(merge([1, 2, 3, 4], [5, 6, 7, 8]))
-print(merge([5, 6, 7, 8], [1, 2, 3, 4]))
-print(merge([4, 7, 8, 9], [1, 3, 6, 10]))
+
 print(consecutive_sum(1, 10))
 
 print("trapping rain")
