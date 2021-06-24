c4e74fa88f62cc1d6f90b855a3b8af5664537230
hjjja <jahj30734@gmail.com>
2021-06-24 23:39:46 +0900

    _

--- hello.py
+++ hello.py
@@ -263,9 +263,6 @@ def merge_sort(my_list):
 
 
 print(merge_sort([1, 3, 5, 7, 9, 11, 13, 11]))
-print(merge_sort([28, 13, 9, 30, 1, 48, 5, 7, 15]))
-print(merge_sort([2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]))
-
 print(merge([1], []))
 print(merge([1, 2, 3, 4], [5, 6, 7, 8]))
 
