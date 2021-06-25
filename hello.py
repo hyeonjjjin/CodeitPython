fb073de2e66e92fe5674092d665a8461b0caf715
hjjja <jahj30734@gmail.com>
2021-06-25 18:22:47 +0900

    Quick partition_fin

--- hello.py
+++ hello.py
@@ -262,6 +262,40 @@ def merge_sort(my_list):
     # 상호반복.. 을 통해 정렬 되는거다!
 
 
+# 두 요소의 위치를 바꿔주는 helper function
+def swap_elements(my_list, index1, index2):
+    swap = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = swap
+
+
+# 퀵 정렬에서 사용되는 partition 함수
+def partition(my_list, start, end):
+    b = i = start
+    while i < end:
+        if my_list[i] > my_list[end]:
+            i += 1
+        else:
+            swap_elements(my_list, i, b)
+            b += 1
+            i += 1
+    swap_elements(my_list, b, end)
+    return b
+
+
+# 테스트 1
+list1 = [4, 3, 6, 2, 7, 1, 5]
+pivot_index1 = partition(list1, 0, len(list1) - 1)
+print(list1)
+print(pivot_index1)
+
+# 테스트 2
+list2 = [6, 1, 2, 6, 3, 5, 4]
+pivot_index2 = partition(list2, 0, len(list2) - 1)
+print(list2)
+print(pivot_index2)
+
+
 print(merge_sort([1, 3, 5, 7, 9, 11, 13, 11]))
 print(merge([1], []))
 print(merge([1, 2, 3, 4], [5, 6, 7, 8]))
