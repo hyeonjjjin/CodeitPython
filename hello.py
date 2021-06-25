61d90aafa2aab956d9fe2f4c9dc4b9d3ec0ff2d7
hjjja <jahj30734@gmail.com>
2021-06-25 23:14:51 +0900

    Quick quicksort2_

--- hello.py
+++ hello.py
@@ -292,20 +292,30 @@ def quicksort(my_list, start, end):
         quicksort(my_list, newpivot+1, end)
 
 
-# 테스트 1
-#list1 = [1, 3, 5, 7, 9, 11, 13, 11]
-#quicksort(list1, 0, len(list1) - 1)
-#print(list1)
+def quicksort2(my_list):
+    if len(my_list) < 1:
+        return my_list
+    newpivot = partition(my_list, 0, len(my_list)-1)
+    if newpivot < 1 or len(my_list)-newpivot < 1:
+        return my_list
+    else:
+        my_list[:newpivot] = quicksort2(my_list[:newpivot])
+        my_list[newpivot+1:] = quicksort2(my_list[newpivot+1:])
+
 
 # 테스트 2
 list2 = [28, 13, 9, 30, 1, 48, 5, 7, 15]
-quicksort(list2, 0, len(list2) - 1)
+quicksort2(list2) # start, end 파라미터 없이 호출
 print(list2)
 
 # 테스트 3
 list3 = [2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]
-quicksort(list3, 0, len(list3) - 1)
+quicksort2(list3) # start, end 파라미터 없이 호출
 print(list3)
+
+list2 = [28, 13, 9, 30, 1, 48, 5, 7, 15]
+quicksort(list2, 0, len(list2) - 1)
+print(list2)
 
 list1 = [4, 3, 6, 2, 7, 1, 5]
 pivot_index1 = partition(list1, 0, len(list1) - 1)
