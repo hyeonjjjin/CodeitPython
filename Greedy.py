00626262b9dfce826294529cc070e5ce3d2a5bf1
hjjja <jahj30734@gmail.com>
2021-07-02 17:18:44 +0900

    max_product _fin

--- Greedy.py
+++ Greedy.py
@@ -9,6 +9,26 @@ def min_coin_count(value, coin_list):
     return count
 
 
+def max_product(card_lists):
+    answer = max(card_lists[0])
+    for i in range(1, len(card_lists)):
+        answer *= max(card_lists[i])
+    return answer
+
+
+# 테스트
+test_cards1 = [[1, 6, 5], [4, 2, 3]]
+print(max_product(test_cards1))
+
+test_cards2 = [[9, 7, 8], [9, 2, 3], [9, 8, 1], [2, 8, 3], [1, 3, 6], [7, 7, 4]]
+print(max_product(test_cards2))
+
+test_cards3 = [[1, 2, 3], [4, 6, 1], [8, 2, 4], [3, 2, 5], [5, 2, 3], [3, 2, 1]]
+print(max_product(test_cards3))
+
+test_cards4 = [[5, 5, 5], [4, 3, 5], [1, 1, 1], [9, 8, 3], [2, 8, 4], [5, 7, 4]]
+print(max_product(test_cards4))
+
 # 테스트
 default_coin_list = [100, 500, 10, 50]
 print(min_coin_count(1440, default_coin_list))
